ca8e49ac07d1edbe5c58812f56e313b27faaa246
Jared Allard <jaredallard@outlook.com>
2015-07-30 13:27:55 -0700

    retrack bower stuff

---
+++ src/js/gitter.js
@@ -0,0 +1,272 @@
+/**
+ * Javascript API Wrapper for gitter.im.
+ *
+ * @author Jared Allard <jaredallard@outollo.com>
+ * @version 0.1.1
+ * @license MIT
+ * @note This was originally designed for Windows Phone
+ *
+ * @class Gitter
+ */
+function Gitter() {
+  // Specific data for the object on construct
+  this.localSettings = {};
+  this.localSettings.values = {};
+  this.apiurl = "https://api.gitter.im/";
+
+  // user options
+  this.localSettings.values.access_token = localStorage.getItem("access_token");
+  this.localSettings.values.loggedin = true;
+}
+
+/**
+ * Make a request with our headers
+ *
+ * @param {string} method - HTTP Method i.e POST, GET
+ * @param {string} endpoint - https://api.gitter.im/.../... API endpoint!
+ * @param {object} options - options for request
+ * @memberOf Gitter
+ */
+Gitter.prototype.request = function (method, endpoint, options) {
+  /* Setup headers */
+  var _headers = {
+      'Authorization': 'Bearer ' + this.localSettings.values.access_token,
+      'Accept':        'application/json'
+    },
+    _data = {},
+    _url = this.apiurl + endpoint;
+
+  // worked for get
+  _url = _url+"?access_token="+this.localSettings.values.access_token;
+
+  // Include the data request if told too.
+  if (typeof (options.params) === "object") {
+    _data = options.params;
+    _headers['Content-Type'] = 'application/json';
+  }
+
+  if (options.url !== undefined) {
+    _url = options.url + endpoint;
+  }
+
+  // data
+  if(_data) {
+    console.log(_data);
+    _data = JSON.stringify(_data);
+  } else {
+    _data = undefined;
+  }
+
+  console.log("Request to " + endpoint);
+  console.log(options);
+  console.log(method + " " + _url);
+
+  console.log(_headers);
+
+  /* Make AJAX request */
+  var result = $.ajax(_url, {
+    method: method,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    data: _data
+  });
+
+  // callbacks
+  result
+    .then(function(data) {
+        options.success(data);
+    });
+};
+
+/*
+ * Symlink to this.request.
+ *
+ * @params {string} endpoint - endpoint of API
+ * @params {object} params - options
+ * @memberOf Gitter
+ */
+Gitter.prototype.get = function (endpoint, params) {
+  this.request("GET", endpoint, params);
+};
+
+/*
+ * Symlink to this.request.
+ *
+ * @params {string} endpoint - endpoint of API
+ * @params {object} params - options
+ * @memberOf Gitter
+ */
+Gitter.prototype.post = function (endpoint, params) {
+  this.request("POST", endpoint, params);
+};
+
+/*
+ * Finalizes oAuth action after we were called.
+ * @memberOf Gitter
+ * @return {bool} success
+ */
+Gitter.prototype.init = function (cb) {
+  function getUrlParams(url) {
+    // http://stackoverflow.com/a/23946023/2407309
+    if (typeof url === 'undefined') {
+      url = window.location.search;
+    }
+    url = url.split('#')[0]; // Discard fragment identifier.
+    var urlParams = {};
+    var queryString = url.split('?')[1];
+    if (!queryString) {
+      if (url.search('=') !== false) {
+        queryString = url;
+      }
+    }
+    if (queryString) {
+      var keyValuePairs = queryString.split('&');
+      for (var i = 0; i < keyValuePairs.length; i++) {
+        var keyValuePair = keyValuePairs[i].split('=');
+        var paramName = keyValuePair[0];
+        var paramValue = keyValuePair[1] || '';
+        urlParams[paramName] = decodeURIComponent(paramValue.replace(/\+/g, ' '));
+      }
+    }
+    return urlParams;
+  }
+
+  /* strip the code response from the earlier oauth response */
+  var code = getUrlParams(this.codeStr).code;
+  this.code = code;
+  this.atcb = cb;
+
+  // Link this to that for external this objects.
+  var that = this;
+
+  /* perform oAuth request for access_token */
+  this.post("oauth/token", {
+    url: "https://gitter.im/login/",
+    success: function (d) { /* data, opt textStatus */
+      that.addTokens(d);
+    },
+    error: function (j, s, e) {
+      console.log("e: " + s + " / " + e);
+    },
+    params: {
+      client_id: this.client_id,
+      client_secret: this.client_secret,
+      code: this.code,
+      redirect_uri: this.redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    auth: false
+  });
+};
+
+/*
+ * Internal callback to save the tokens.
+ * @memberOf Gitter
+ */
+Gitter.prototype.addTokens = function (resp) {
+  // save state
+  this.localSettings.values.access_token = resp.access_token;
+  this.localSettings.values.loggedin = true;
+
+  // call our callback
+  this.atcb();
+};
+
+/**
+ * Logs the currently loggedin user out
+ * @memberOf Gitter
+ */
+Gitter.prototype.logout = function () {
+  this.localSettings.values.loggedin = false; // one line
+};
+
+/*
+ * Get users subscribed rooms
+ *
+ * @memberOf Gitter
+ * @callback cb - Callback
+ */
+Gitter.prototype.rooms = function (cb) {
+  this.request("GET", "v1/rooms", {
+    success: function (data) {
+      cb(data);
+    },
+    error: function (j, s, e) {
+      cb(null, { jqXHR: j, textStatus: s, errorThrown: e });
+    },
+    auth: false
+  });
+};
+
+/**
+ * Attempt to get messages from a room
+ *
+ * @param {string} roomId - id of room
+ * @param {integer} limit - limit of messages to get
+ * @param {function} cb - callback to excute on error/success
+ * @param {string} beforeId - before id of messages. optional
+ *
+ * @memberOf Gitter
+ */
+Gitter.prototype.messages = function (roomId, limit, cb, beforeId) {
+  // Setup _params
+  var _params = {};
+  _params.limit = limit;
+
+  // include beforeId if it was defined.
+  if (beforeId !== undefined) {
+    _params.beforeId = beforeId;
+  }
+
+  /* Initiate the request */
+  this.get("v1/rooms/" + roomId + '/chatMessages', {
+    success: function (data) {
+      cb(data);
+    },
+    error: function (j, s, e) {
+      cb(null, { jqXHR: j, textStatus: s, errorThrown: e });
+    },
+    params: _params
+  });
+};
+
+/**
+ * Get an object of the current user who is logged in.
+ *
+ * @memberOf Gitter
+ * @param {function} cb - callback function on error/success
+ */
+Gitter.prototype.whoami = function (cb) {
+  this.request("GET", "v1/user", {
+    success: function (data) {
+      cb(data);
+    },
+    error: function (j, s, e) {
+      cb(null, { jqXHR: j, textStatus: s, errorThrown: e });
+    }
+  });
+};
+
+/**
+ * Post a message to a room from currently logged in user
+ *
+ * @memberOf Gitter
+ * @param {string} rid - id of room
+ * @param {string} text - text to post
+ * @param {function} cb - callback on error/success
+ */
+Gitter.prototype.sendMessage = function (rid, text, cb) {
+  return this.post('v1/rooms/' + rid + '/chatMessages', {
+    success: function (data) {
+      cb(data);
+    },
+    error: function (j, s, e) {
+      cb(null, { jqXHR: j, textStatus: s, errorThrown: e });
+    },
+    params: {
+      text: text
+    }
+  });
+};
---
+++ src/js/main.js
@@ -0,0 +1,385 @@
+/**
+ * gtrdeck js
+ **/
+
+ "use strict";
+
+// load gitter
+var gtr = new Gitter();
+
+// login page register.
+page.register({
+  name: "login",
+  onBack: false,
+  init: function() {
+    $(".login-panel").height(170);
+    $(".login-hello, #access_token").show();
+    $("article[page=login]").show();
+  },
+  exit: function() {
+    $("article[page=login]").hide();
+  },
+  nav: true,
+  back: false
+});
+
+// index "tab" page.
+page.register({
+  name: "index",
+  onBack: false,
+  init: function() {
+    $("html,body").attr("style", "background-image:none !important;background-color: #444448 !important;");
+    $("article[page=index]").show();
+
+    console.log(localStorage.getItem("access_token")); // verify we have a true at
+
+    initFaye(localStorage.getItem("access_token"));
+
+    newMessageHighlights();
+
+    $('.message-textarea').keydown(function (e) {
+
+      if (e.ctrlKey && e.keyCode == 13) {
+        sendMessage();
+      }
+    });
+
+    // time stream
+    setInterval(function() {
+      $(".sentTime").timeago();
+    }, 5000);
+  },
+  exit: function() {
+    $("article[page=index]").hide();
+  },
+  nav: false,
+  back: false
+});
+
+// page is loading page
+page.register({
+  name: 'load',
+  onBack: false,
+  init: function() {
+    var opts = {
+       lines: 13, // The number of lines to draw
+       length: 28, // The length of each line
+       width: 14, // The line thickness
+       radius: 42, // The radius of the inner circle
+       scale: 1, // Scales overall size of the spinner
+       corners: 1, // Corner roundness (0..1)
+       color: '#FFFFFF', // #rgb or #rrggbb or array of colors
+       opacity: 0, // Opacity of the lines
+       rotate: 0, // The rotation offset
+       direction: 1, // 1: clockwise, -1: counterclockwise
+       speed: 1, // Rounds per second
+       trail: 30, // Afterglow percentage
+       fps: 20, // Frames per second when using setTimeout() as a fallback for CSS
+       zIndex: 2e9, // The z-index (defaults to 2000000000)
+       className: 'spinner', // The CSS class to assign to the spinner
+       top: '50%', // Top position relative to parent
+       left: '50%', // Left position relative to parent
+       shadow: false, // Whether to render a shadow
+       hwaccel: true, // Whether to use hardware acceleration
+       position: 'absolute' // Element positioning
+    };
+
+    $(".load-wrapper").spin(opts);
+
+    $("article[page=load]").show();
+  },
+  exit: function() {
+    $("article[page=load]").hide();
+  },
+  nav: true,
+  back: false
+});
+
+// inital loading page
+page.set("load");
+
+function getRoomMessages(rid) {
+  gtr.messages(rid, 50, function (data, err) {
+    data.forEach(function (v) {
+      generateMessage(v, rid);
+    });
+
+    // resize hook.
+    $(".tab .content-wrapper").height($(".tab").height()-50);
+
+    $(document).ready(function() {
+      $('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
+      });
+    });
+
+    // start message stream
+    streamRoom(rid);
+  });
+}
+
+function generateMessage(v, rid) {
+  // precompile the template.
+  if(!window.message_template) {
+    var source   = $("#messageTemplate").html();
+    window.message_template = Handlebars.compile(source);
+  }
+
+  var text = marked(v.text);
+
+  // usernames
+  text = text.replace(/\@([a-zA-Z]+)/g,
+  "<a href='https://github.com/$1' target='__blank'>@$1</a>");
+
+  var name = v.fromUser.displayName.substr(0,17);
+  if(name.length===17 && v.fromUser.displayName.length!==17) {
+    name = name+" <b>...</b>";
+  }
+
+  // handlebars template.
+  var s = window.message_template({
+    img: v.fromUser.avatarUrlMedium,
+    name: name,
+    at: v.fromUser.username,
+    contents: text,
+    sentTime: v.sent,
+    sentTimeParsed: $.timeago(v.sent),
+    id: v.id
+  });
+
+  $("#"+rid+"-content").prepend(s);
+  $("#"+rid+"-content").show();
+}
+
+function genTab(obj) {
+  if(window.tab_template===undefined) { // check if already compilied
+    var source          = $("#tabTemplate").html();
+    window.tab_template = Handlebars.compile(source);
+  }
+
+  // check if tab already exists.
+  if(document.getElementById(obj.rid+"-tab")) {
+    console.log("[tab] obj: ", document.getElementById(obj.rid+"-tab"));
+    console.log("[tab] ignorning request to make new tab, already exists.");
+    return false;
+  }
+
+  var str = "<div class='tabs'><img src='"+obj.img+"' /></div>";
+  var comp = window.tab_template(obj);
+
+  console.log("[tab]: new tab", obj);
+
+  $(".tab-wrapper").append(comp);
+  $(".tabs-wrapper").append(str);
+
+  $('#'+obj.rid+'-tab .header').click(function() {
+    setMessageScope($(this).attr('data-rid'));
+  });
+
+  // get messages.
+  getRoomMessages(obj.rid);
+}
+
+function addTabs() {
+  $(".new-tabs").html("");
+  $(".new-tabs").spin("large", "#444448");
+
+  // fill room dialog with rooms.
+  gtr.rooms(function(data, err) {
+    // precompile the room template; if needed.
+    if(!window.room_template) {
+      var source   = $("#roomTemplate").html();
+      window.room_template = Handlebars.compile(source);
+    }
+
+    $(".new-tabs").spin(false); // stop the spinner
+
+    window.rooms = {};
+
+    data.forEach(function(v) {
+      window.rooms[v.id] = v; // add for other stuff.
+      var comp = window.room_template({
+        id: v.id,
+        name: v.name,
+        img: "https://avatars.githubusercontent.com" + "/" + v.name.split("/")[0]
+      });
+
+      $(".new-tabs").append(comp);
+    });
+
+    $(".room").click(function() {
+      var roomname = $(this).attr("roomname");
+      var roomid = $(this).attr("roomid");
+      var roomimg = $(this).attr("roomimg");
+
+      // make the tab.
+      genTab({
+        rid: roomid,
+        name: roomname,
+        img: roomimg
+      });
+
+      // close the dialog.
+      closeTabDialog();
+    });
+  });
+
+  $(".tab-add-dialog").show();
+  $(".dialog-bg").show();
+}
+
+function closeTabDialog() {
+  $(".tab-add-dialog").hide();
+  $(".dialog-bg").hide();
+}
+
+function initFaye(token) {
+  // Authentication extension
+  var ClientAuthExt = function() {};
+  ClientAuthExt.prototype.outgoing = function(message, callback) {
+    if (message.channel === '/meta/handshake') {
+      if (!message.ext) { message.ext = {}; }
+      message.ext.token = token;
+    }
+
+    callback(message);
+  };
+
+  ClientAuthExt.prototype.incoming = function(message, callback) {
+    if(message.channel === '/meta/handshake') {
+      if(message.successful) {
+        console.log('Successfuly subscribed to room: ', roomId);
+      } else {
+        console.log('Something went wrong: ', message.error);
+      }
+    }
+
+    callback(message);
+  };
+
+  // Snapshot extension
+  var SnapshotExt = function() {};
+  ClientAuthExt.prototype.incoming = function(message, callback) {
+    if(message.channel === '/meta/subscribe' && message.ext && message.ext.snapshot) {
+      console.log('Snapshot: ', message.ext.snapshot);
+    }
+
+    callback(message);
+  };
+
+  // Faye client
+  window.faye_client = new Faye.Client('https://ws.gitter.im/faye', {timeout: 60, retry: 5, interval: 1});
+
+  // Add Client Authentication extension
+  window.faye_client.addExtension(new ClientAuthExt());
+
+  // A dummy handler to echo incoming messages
+  window.messageHandler = function(msg, rid) {
+    if (msg.operation) {
+      // new message.
+      if(msg.operation === "create") {
+        console.log("[message]: ", msg);
+        generateMessage(msg.model, rid);
+      }
+    } else if(msg.notification) {
+      console.log("[notification]: ", msg);
+    } else {
+      console.log("[faye] unregistered type: ", msg);
+    }
+  };
+}
+
+function streamRoom(rid) {
+  console.log("[faye] register onto room: "+rid);
+  function mh(msg) {
+    var _rid = rid;
+    window.messageHandler(msg, _rid);
+  }
+
+  window.faye_client.subscribe('/api/v1/rooms/' + rid,                   mh, {});
+  window.faye_client.subscribe('/api/v1/rooms/' + rid + '/chatMessages', mh, {});
+  window.faye_client.subscribe('/api/v1/rooms/' + rid + '/users',        mh, {});
+  window.faye_client.subscribe('/api/v1/rooms/' + rid + '/events',       mh, {});
+
+  console.log("[faye] subscribed.");
+}
+
+function newMessage() {
+  $('.message-wrapper').toggle();
+}
+
+function newMessageHighlights() {
+  $('.message-textarea').on('keyup', function() {
+    var contents = $(this).text();
+    var at = contents.match(/\@([a-zA-Z0-9]+)/g);
+
+    console.log(at);
+  });
+
+  setInterval(function(){
+    $('.message-text-counter').text($('.message-textarea').text().length);
+  }, 10);
+}
+
+function setMessageScope(rid) {
+  window.message_scope = rid; // temp?
+
+
+  if(window.rooms[rid] !== undefined) {
+    console.log(window.rooms[rid].name);
+    $(".message-room-img").attr('src', "https://avatars.githubusercontent.com" + "/" + window.rooms[rid].name.split("/")[0])
+    $(".message-room-name").text(window.rooms[rid].name);
+    $(".message-room-img").show();
+  } else {
+    console.log("Error: No name for room registered, will display reply as ID.")
+    $(".message-room-name").text(rid);
+  }
+
+   $('#message-btn').attr("disabled", false);
+}
+
+function doLogin(at) {
+  if(localStorage.getItem("access_token")!==undefined && localStorage.getItem("access_token")!==null) {
+       page.set('index');
+       return false; // refuse to run if already set.
+  }
+
+  localStorage.setItem("access_token", at);
+  gtr.localSettings.values.access_token = localStorage.getItem("access_token");
+  page.set('index');
+}
+
+function sendMessage() {
+  if(window.message_scope === undefined) {
+    return false;
+  }
+
+  var message = $(".message-textarea").text();
+  $('.message-textarea').attr("disabled", true);
+
+  // send the message
+  gtr.sendMessage(window.message_scope, message, function(data) {
+    if(data.html === undefined) {
+      console.log("Failed to send message");
+    } else {
+      $('.message-textarea').html("");
+    }
+
+    $('.message-textarea').attr("disabled", false);
+  });
+}
+
+function resize() {
+  $(".tab .content-wrapper").height($(".tab").height()-50);
+}
+$(window).on('resize', resize);
+
+// initial page or to index.
+if(localStorage.getItem("access_token")!==undefined && localStorage.getItem("access_token")!==null) {
+  gtr.whoami(function(data) {
+    window.user_object = data[0]; // user data object.
+    page.set("index");
+  });
+} else {
+  page.set("login");
+}
